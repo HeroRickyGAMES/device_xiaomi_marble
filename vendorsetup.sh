2945ef60c32330dd465b46add248671069e2af08
yogesh <101273058+pabloescobar-reborn@users.noreply.github.com>
2024-11-15 19:50:20 +0530

    useless commit: Update vendorsetup.sh to fifteen

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,6 @@
 echo 'Hello! we need to clone stuffs needed for your device, please wait tll process complete'
 
 echo 'Nuke Source stuffs'
-	rm -rf device/xiaomi
-        rm -rf vendor/xiaomi
-        rm -rf vendor/bcr
-        rm -rf kernel/xiaomi
         rm -rf hardware/xiaomi
         rm -rf hardware/qcom-caf/sm8450/display
 
@@ -12,7 +8,7 @@ echo 'Cloning Basic Call Recorder Tree'
 	git clone https://github.com/Chaitanyakm/vendor_bcr vendor/bcr
 
 echo 'Cloning Device Common Tree'
-	git clone https://github.com/pabloescobar-reborn/device_xiaomi_sm8450-common -b fourteen device/xiaomi/sm8450-common
+	git clone https://github.com/pabloescobar-reborn/device_xiaomi_sm8450-common -b fifteen device/xiaomi/sm8450-common
 
 echo 'Cloning MemeCamera Tree'
 	git clone https://github.com/pabloescobar-reborn/device_xiaomi_miuicamera-marble -b fourteen device/xiaomi/miuicamera-marble
@@ -22,7 +18,7 @@ echo 'Cloning Hardware Display Tree'
 	git clone https://github.com/pabloescobar-reborn/qcom-caf_display -b fourteen hardware/qcom-caf/sm8450/display
 
 echo 'Cloning Hardware Xiaomi Tree'
-	git clone https://github.com/pabloescobar-reborn/hardware_xiaomi hardware/xiaomi
+	git clone https://github.com/pabloescobar-reborn/hardware_xiaomi -b fifteen hardware/xiaomi
 
 echo 'Cloning Kernel Tree'
 	git clone --depth=1 https://github.com/pabloescobar-reborn/kernel_xiaomi_sm8450_chai -b udc kernel/xiaomi/sm8450
@@ -30,7 +26,7 @@ echo 'Cloning Kernel Tree'
 	git clone --depth=1 https://github.com/pabloescobar-reborn/chai_kernel_xiaomi_sm8450-devicetrees -b main kernel/xiaomi/sm8450-devicetrees
 
 echo 'Cloning Vendor Tree'
-	git clone https://github.com/pabloescobar-reborn/vendor_xiaomi_marble -b fourteen vendor/xiaomi/marble
+	git clone https://github.com/pabloescobar-reborn/vendor_xiaomi_marble -b fifteen vendor/xiaomi/marble
 
 echo 'Cloning Vendor Common Tree'
-	git clone https://github.com/pabloescobar-reborn/vendor_xiaomi_sm8450-common -b fourteen vendor/xiaomi/sm8450-common
+	git clone https://github.com/pabloescobar-reborn/vendor_xiaomi_sm8450-common -b fifteen vendor/xiaomi/sm8450-common
